a87c2b9f45beee7fa52acd352ae191d50779a679
sunksun <sunksunlapunt@gmail.com>
2023-11-10 11:48:03 +0700

    chang1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          นายสังสรรค์ หล้าพันธ์
+          sunksun lapunt
         </p>
         <a
           className="App-link"
